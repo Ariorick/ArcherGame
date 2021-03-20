b9c36272ec9c662d03c68762b2fffac70b111d28
Sergey <skorneevsky@okko.tv>
2021-03-21 01:02:09 +0300

    added randomly generate map

---
+++ src/Level/World.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+const YELLOW_CAP = 0.3
+const GREEN_CAP = 0.62
+const GRAVE_CAP = -0.3
+
+var rng = RandomNumberGenerator.new()
+
+var noise
+var secondary_noise
+var map_size = Vector2()
+
+func _ready():
+	randomize()
+	noise = OpenSimplexNoise.new()
+	noise.seed = randi()
+	noise.persistence = 0.7
+	noise.octaves = 1.0
+	noise.period = 12
+	randomize()
+	secondary_noise = OpenSimplexNoise.new()
+	secondary_noise.seed = randi()
+	secondary_noise.persistence = 0.7
+	secondary_noise.octaves = 1.0
+	secondary_noise.period = 12
+	
+	
+#	$Grass.cell_custom_transform = transform
+
+func generate(chunk: Vector2):
+	var from = chunk * map_size
+	var to = (chunk + Vector2(1, 1)) * map_size
+	run_with_noise(from, to, funcref(self, "make_grass_map"))
+	run_with_noise(from, to, funcref(self, "make_graves_map"))
+
+func make_grass_map(x, y, value):
+	if value > GREEN_CAP:
+		$Grass.set_cell(x,y,1)
+	elif value > YELLOW_CAP:
+		$Grass.set_cell(x,y,2)
+	else:
+		$Grass.set_cell(x,y,0)
+
+func make_graves_map(x, y, value):
+	if value < GRAVE_CAP:
+		var can_place = x % 2 + y % 2 == 0
+		if can_place:
+			var random_grave = rng.randi_range(2, 3)
+			$Graves.set_cell(x, y, random_grave)
+
+func run_with_noise(from: Vector2, to: Vector2, generate_fun: FuncRef):
+	for x in range(from.x, to.x):
+		for y in range(from.y, to.y):
+			var value = noise.get_noise_2d(x,y)
+			generate_fun.call_func(x, y, value)
+			$Grass.update_bitmask_region(from, to)
+
+func update_regions(from: Vector2, to: Vector2):
+	for child in get_children(): 
+		child.update_bitmask_region(from, to)
+
+func get_player():
+	return $Graves/Player
---
+++ src/Main.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+var prev_camera_pos = Vector2()
+
+var map_size := Vector2(10, 10)
+var cell_size_px := Vector2(16, 16)
+var chunk_size_px := map_size * cell_size_px
+var map_cells := Dictionary()
+var lookaround := Vector2()
+
+func _ready():
+	$World.map_size = map_size
+	
+#	var camera_zoom = $CameraPosition/Camera2D.zoom
+	var camera_zoom = 1
+	var screen_size = get_viewport().size * camera_zoom
+	
+	lookaround = Vector2(
+		int(screen_size.x / chunk_size_px.x) / 2 + 2,
+		int(screen_size.y / chunk_size_px.y) / 2 + 2
+		)
+		
+	$World.get_player().position = chunk_size_px / 2
+
+
+func _process(delta):
+	var pos = $World.get_player().position
+#	$CameraPosition.position = pos
+	generate_around(pos)
+
+
+func generate_around(pos: Vector2):
+	var chunk : = Vector2(
+		int(pos.x) / int(chunk_size_px.x), 
+		int(pos.y) / int(chunk_size_px.y)
+		)
+	
+	for x in range(chunk.x - lookaround.x, chunk.x + lookaround.x):
+		for y in range(chunk.y - lookaround.y, chunk.y + lookaround.y):
+			generate_chunk(Vector2(x, y))
+	
+
+
+func generate_chunk(chunk: Vector2): 
+	if map_cells.has(chunk) && map_cells[chunk] == true:
+		return
+	$World.generate(chunk)
+	map_cells[chunk] = true
+	
+	
+	
