aa146a8421c805cb77f43cb7ed7b3eee80d19432
Sergey <skorneevsky@okko.tv>
2021-10-26 13:00:48 +0300

    now removing save file efor writing into it

--- src/gameManager/Saver.gd
+++ src/gameManager/Saver.gd
@@ -1,14 +1,16 @@
 extends Node
 # SAVER
 
+const SAVE_FILE = "user://savegame.save"
+
 func _ready():
 	load_game()
 
 func load_game():
 	var save_game = File.new()
-	if not save_game.file_exists("user://savegame.save"):
+	if not save_game.file_exists(SAVE_FILE):
 		return # Error! We don't have a save to load.
-	save_game.open("user://savegame.save", File.READ)
+	save_game.open(SAVE_FILE, File.READ)
 	
 	Inventory.set_items(parse_json(save_game.get_line()))
 	
@@ -16,8 +18,11 @@ func load_game():
 	save_game.close()
 
 func save_game():
+	var dir = Directory.new()
+	if not dir.file_exists(SAVE_FILE):
+		dir.remove(SAVE_FILE)
 	var save_game = File.new()
-	save_game.open("user://savegame.save", File.WRITE)
+	save_game.open(SAVE_FILE, File.WRITE)
 	
 	save_game.store_line(to_json(Inventory.items))
 	
